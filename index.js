61a055b6ae7d88f3d5d07a945d8f3e7a6278a81a
ashutoshPanda <iamashutoshpanda@gmail.com>
2024-02-03 19:47:54 +0530

    added sentry

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ import routes from "./src/routes/index.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import setup from "./db.js";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 const app = express();
+Sentry.init({
+  dsn: process.env.SENTRY_BACKEND_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // mongo db setup
 setup();
@@ -16,6 +38,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Here we define the api routes
 app.use(routes);
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // Routes
 app.get("/", (req, res) => {
